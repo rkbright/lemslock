a197038251eb1ae831fe1714f389d77342c74e68
richard bright <rkbright1@gmail.com>
2021-11-20 17:01:33 -0800

    added chi for routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rkbright/lenslock
 
 go 1.14
+
+require github.com/go-chi/chi/v5 v5.0.7 // indirect
--- main.go
+++ main.go
@@ -15,32 +15,34 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:rkbright1@gmail.com\">rkbright1@gmail.com</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1><p>Common questions asked</p> 
+				  <ul> 
+					 <li><b>Is there a free version?</b> Yes! We offer a free trial version on any paid plans.</li>
+					 <li>What are your support hours?</li>
+					 <li>How do we contact support?</li>
+				  </ul>`)
+}
+
+//Router routs page requests to the correct function
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	}
 }
 
-//Router routs page requests to the correct function
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page Not Found", http.StatusNotFound)
-// 	}
-// }
-
 func main() {
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", router)
 }
